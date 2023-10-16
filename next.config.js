ac09ff7c68d7af5ed22355ea77dc340e29352f82
Vincent_lok <vincent200913@gmail.com>
2023-10-17 01:22:56 +0800

    deploy: add basePath in nextjs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/typewriter-generator",
   reactStrictMode: true,
   images: {
     unoptimized: true,
